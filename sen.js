f06c77c46a6df2b86559e0a61c7e36b635549837
Fadhil Pratama Siregar <115153818+Fadhilsrg@users.noreply.github.com>
2023-11-29 12:40:06 +0700

    Add files via upload

---
+++ sen.js
@@ -0,0 +1,587 @@
+// lirik
+const lyricsData = {
+  Play1: `I'm going back to 505
+    If it's a seven hour flight or a forty-five minute drive
+    In my imagination, you're waitin' lyin' on your side
+    With your hands between your thighs
+
+    Stop and wait a sec
+    When you look at me like that, my darlin', what did you expect?
+    I'd probably still adore you with your hands around my neck
+    Or I did last time I checked
+
+    Not shy of a spark
+    The knife twists at the thought that I should fall short of the mark
+    Frightened by the bite, though it's no harsher than the bark
+    The middle of adventure, such a perfect place to start
+    
+    I'm going back to 505
+    If it's a seven hour flight or a forty-five minute drive
+    In my imagination, you're waitin' lyin' on your side
+    With your hands between your thighs
+
+    But I crumble completely when you cry
+    It seems like once again you've had to greet me with goodbye
+    I'm always just about to go and spoil the surprise
+    Take my hands off of your eyes too soon
+
+    I'm going back to 505
+    If it's a seven hour flight or a forty-five minute drive
+    In my imagination, you're waitin' lyin' on your side
+    With your hands between your thighs and a smile`,
+
+  Play2: `Shoot me
+    Shoot me
+    Shoot me
+    Shoot me
+
+    Here come old flat top
+    He come groovin' up slowly
+    He got ju-ju eyeball
+    He one holy roller
+    He got hair down to his knee
+    Got to be a joker
+    He just do what he please
+
+    Shoot me
+    Shoot me
+    Shoot me
+    Shoot me
+
+    He wear no shoeshine
+    He got toe jam football
+    He got monkey finger
+    He shoot Coca-Cola
+    He say I know you, you know me
+    One thing I can tell you is
+    You got to be free
+    Come together, right now
+    Over me
+
+    Shoot me
+    Shoot me
+    Shoot me
+
+    He bag production
+    He got walrus gumboot
+    He got Ono sideboard
+    He one spinal cracker
+    He got feet down below his knee
+    Hold you in his armchair
+    You can feel his disease
+    Come together, right now
+    Over me
+
+    Shoot me
+    Right
+
+    Come, come, come, come, come
+
+    He roller coaster
+    He got early warning
+    He got muddy water
+    He one mojo filter
+    He say one and one and one is three
+    Got to be good looking
+    'Cause he's so hard to see
+    Come together right now
+    Over me
+
+    Shoot me
+    Shoot me
+    Shoot me
+    Uh
+
+    Come together, yeah
+    Come together, yeah
+    Come together, yeah
+    Come together, yeah
+    Come together, yeah
+    Come together, yeah
+    Come together, yeah
+    Uh
+    Come together, yeah
+    Come together, yeah`,
+
+  Play3: `Did I drive you away?
+    I know what you'll say
+    You say, "Oh, sing one we know"
+
+    But I promise you this
+    I'll always look out for you
+    Yeah, that's what I'll do
+
+    I say, "Ohh"
+    I say, "Ohh"
+
+    My heart is yours
+    It's you that I hold on to
+    Yeah, that's what I do
+
+    And I know, I was wrong
+    But I won't let you down
+    Oh, yeah, yeah, yeah, I will, yes, I will
+
+    I said, "Ohh"
+    I cry, "Ohh"
+
+    Yeah, I saw sparks
+    Yeah, I saw sparks
+    And I saw sparks
+    Yeah, I saw sparks
+    Sing it out`,
+
+  Play4: `I'm in my bed
+    And you're not here
+    And there's no one to blame but the drink in my wandering hands
+    Forget what I said
+    It's not what I meant
+    And I can't take it back, I can't unpack the baggage you left
+
+    What am I now? What am I now?
+    What if I'm someone I don't want around?
+    I'm falling again, I'm falling again, I'm falling
+    What if I'm down? What if I'm out?
+    What if I'm someone you won't talk about?
+    I'm falling again, I'm falling again, I'm falling
+
+    You said you care
+    And you missed me too
+
+    And I'm well aware I write too many songs about you
+    And the coffee's out
+    At the Beachwood Cafe
+    And it kills me 'cause I know we've run out of things we can say
+
+    What am I now? What am I now?
+    What if I'm someone I don't want around?
+    I'm falling again, I'm falling again, I'm falling
+    What if I'm down? What if I'm out?
+    What if I'm someone you won't talk about?
+    I'm falling again, I'm falling again, I'm falling
+
+    And I get the feeling that you'll never need me again
+
+    What am I now? What am I now?
+    What if you're someone I just want around
+    I'm falling again, I'm falling again, I'm falling
+    What if I'm down? What if I'm out?
+    What if I'm someone you won't talk about?
+    I'm falling again, I'm falling again, I'm falling`,
+
+  Play5: `Youngblood thinks there's always tomorrow
+I miss your touch on nights when I'm hollow
+I know you crossed a bridge that I can't follow
+
+Since the love that you left is all that I get
+I want you to know
+
+That if I can't be close to you
+I'll settle for the ghost of you
+I miss you more than life (more than life)
+And if you can't be next to me
+Your memory is ecstasy
+I miss you more than life
+I miss you more than life
+
+Youngblood thinks there's always tomorrow
+I need more time but time can't be borrowed
+I'd leave it all behind if I could follow
+
+Since the love that you left is all that I get
+I want you to know
+
+That if I can't be close to you
+I'll settle for the ghost of you
+I miss you more than life (yeah)
+And if you can't be next to me
+Your memory is ecstasy (oh)
+I miss you more than life
+I miss you more than life
+
+Whoa
+Na, na-na
+More than life
+(Oh)
+
+So if I can't get close to you
+I'll settle for the ghost of you
+But I miss you more than life
+And if you can't be next to me
+Your memory is ecstasy
+I miss you more than life
+I miss you more than life`,
+
+  Play6: `I'm so happy 'cause today I found my friends
+  They're in my head
+  I'm so ugly, that's okay, 'cause so are you
+  Broke our mirrors
+  Sunday morning is everyday, for all I care
+  And I'm not scared
+  Light my candles in a daze
+  'Cause I've found God
+
+  Yeah, yeah
+  Yeah, yeah
+  Yeah, yeah
+  Yeah, yeah
+  Yeah, yeah
+  Yeah, yeah, yeah
+
+  I'm so lonely, that's okay, I shaved my head
+  And I'm not sad
+  And just maybe I'm to blame for all I've heard
+  But I'm not sure
+  I'm so excited, I can't wait to meet you there
+  And I don't care
+  I'm so horny, that's okay
+  My will is good
+
+  Yeah, yeah
+  Yeah, yeah
+  Yeah, yeah
+  Yeah, yeah
+  Yeah, yeah
+  Yeah, yeah, yeah
+
+  I like it, I'm not gonna crack
+  I miss you, I'm not gonna crack
+  I love you, I'm not gonna crack
+  I killed you, I'm not gonna crack
+  I like it, I'm not gonna crack
+  I miss you, I'm not gonna crack
+  I love you, I'm not gonna crack
+  I killed you, I'm not gonna crack
+
+  I'm so happy 'cause today I found my friends
+  They're in my head
+  I'm so ugly, that's okay, 'cause so are you
+  Broke our mirrors
+  Sunday morning is everyday, for all I care
+  And I'm not scared
+  Light my candles in a daze
+  'Cause I've found God
+
+  Yeah, yeah
+  Yeah, yeah
+  Yeah, yeah
+  Yeah, yeah
+  Yeah, yeah
+  Yeah, yeah, yeah
+  
+  I like it, I'm not gonna crack
+  I miss you, I'm not gonna crack
+  I love you, I'm not gonna crack
+  I killed you, I'm not gonna crack
+  I like it, I'm not gonna crack
+  I miss you, I'm not gonna crack
+  I love you, I'm not gonna crack
+  I killed you, I'm not gonna crack`,
+
+  Play7: `Walking Scott Street, feeling like a stranger
+  With an open heart, open container
+  I've got a stack of mail and a tall can
+  It's a shower beer, it's a payment plan
+  There's helicopters over my head
+  Every night when I go to bed
+  Spending money and I earned it
+  When I'm lonely, that's when I'll burn it
+
+  Do you feel ashamed
+  When you hear my name?
+
+  I asked you, "How is your sister?
+  I heard she got her degree"
+  And I said, "That makes me feel old"
+  You said, "What does that make me?"
+  I asked you, "How is playing drums?"
+  You said, "It's too much shit to carry"
+  "And what about the band?"
+  You said, "They're all getting married"
+  
+  Do you feel ashamed
+  When you hear my name?
+  Anyway, don't be a stranger
+  Anyway, don't be a stranger
+  Don't be a stranger`,
+
+  Play8: `And disciplinary remains mercifully
+  Yes and um, I'm with you Derek, this star nonsense
+  Yes, yes
+  Now which is it?
+  I am sure of it
+
+  So, so you think you can tell
+  Heaven from hell?
+  Blue skies from pain?
+  Can you tell a green field
+  From a cold steel rail?
+  A smile from a veil?
+  Do you think you can tell?
+
+  Did they get you to trade
+  Your heroes for ghosts?
+  Hot ashes for trees?
+  Hot air for a cool breeze?
+  Cold comfort for change?
+  Did you exchange
+  A walk-on part in the war
+  For a lead role in a cage?
+  
+  How I wish, how I wish you were here
+  We're just two lost souls
+  Swimming in a fish bowl
+  Year after year
+  Running over the same old ground
+  What have we found?
+  The same old fears
+  Wish you were here`,
+
+  Play9: `Is this the real life? Is this just fantasy?
+  Caught in a landslide, no escape from reality
+  Open your eyes, look up to the skies and see
+  I'm just a poor boy, I need no sympathy
+  Because I'm easy come, easy go, little high, little low
+  Any way the wind blows doesn't really matter to me, to me
+
+  Mama, just killed a man
+  Put a gun against his head, pulled my trigger, now he's dead
+  Mama, life had just begun
+  But now I've gone and thrown it all away
+  Mama, ooh, didn't mean to make you cry
+  If I'm not back again this time tomorrow
+  Carry on, carry on as if nothing really matters
+
+  Too late, my time has come
+  Sends shivers down my spine, body's aching all the time
+  Goodbye, everybody, I've got to go
+  Gotta leave you all behind and face the truth
+  Mama, ooh (any way the wind blows)
+  I don't wanna die
+  I sometimes wish I'd never been born at all
+
+  I see a little silhouetto of a man
+  Scaramouche, Scaramouche, will you do the Fandango?
+  Thunderbolt and lightning, very, very frightening me
+  (Galileo) Galileo, (Galileo) Galileo, Galileo Figaro, magnifico
+  But I'm just a poor boy, nobody loves me
+  He's just a poor boy from a poor family
+  Spare him his life from this monstrosity
+
+  Easy come, easy go, will you let me go?
+  بِسْمِ ٱللَّٰهِ
+  No, we will not let you go (let him go)
+  بِسْمِ ٱللَّٰهِ
+  We will not let you go (let him go)
+  بِسْمِ ٱللَّٰهِ
+  We will not let you go (let me go)
+  Will not let you go (let me go)
+  Never, never, never, never let me go
+  No, no, no, no, no, no, no
+
+  Oh, mamma mia, mamma mia
+  Mamma mia, let me go
+  Beelzebub has a devil put aside for me, for me, for me
+
+  So you think you can stone me and spit in my eye?
+  So you think you can love me and leave me to die?
+  Oh, baby, can't do this to me, baby
+  Just gotta get out, just gotta get right outta here
+  
+  Ooh
+  Ooh, yeah, ooh, yeah
+  Nothing really matters, anyone can see
+  Nothing really matters
+  Nothing really matters to me`,
+};
+
+for (var key in lyricsData) {
+  lyricsData[key] = lyricsData[key].replace(/\n/g, "<br>");
+}
+
+function tampilkanLirik(buttonId) {
+  var lirikElement = document.querySelector(".tekslirik");
+  lirikElement.innerHTML = lyricsData[buttonId] || "Lirik tidak ditemukan.";
+}
+
+//gambar
+var gambarData = {
+  Play1: "FigmaAlbum/ArcticMonkey.jpg",
+  Play2: "FigmaAlbum/Beatles.jpg",
+  Play3: "FigmaAlbum/Coldplay.jpg",
+  Play4: "FigmaAlbum/HarryStyle.jpg",
+  Play5: "FigmaAlbum/Justin Bieber.jpg",
+  Play6: "FigmaAlbum/Nirvana.jpg",
+  Play7: "FigmaAlbum/Phoebe Bridge.jpg",
+  Play8: "FigmaAlbum/PinkFloyd.jpg",
+  Play9: "FigmaAlbum/Queen.jpg",
+};
+
+function tampilkanGambar(buttonId) {
+  var gambarElement = document.querySelector(".imgMaster img");
+  gambarElement.src = gambarData[buttonId] || "default.jpg";
+}
+
+//title
+var judulData = {
+  Play1: "505",
+  Play2: "Come Together",
+  Play3: "Sparks",
+  Play4: "Falling",
+  Play5: "Ghost",
+  Play6: "Lithium",
+  Play7: "Scott Street",
+  Play8: "Wish You Were Here",
+  Play9: "Bohemian Rhapsody",
+};
+
+function tampilkanJudul(buttonId) {
+  var judulElement = document.querySelector(".judul_mp");
+  judulElement.innerHTML = judulData[buttonId] || "Judul tidak ditemukan.";
+}
+
+//artis
+var artisData = {
+  Play1: "Arctic Monkey",
+  Play2: "The Beatles",
+  Play3: "Coldplay",
+  Play4: "Harry Styles",
+  Play5: "Justin Bieber",
+  Play6: "Nirvana",
+  Play7: "Phoebe Bridgers",
+  Play8: "Pink Floyd",
+  Play9: "Queen",
+};
+
+function tampilkanArtis(buttonId) {
+  var artisElement = document.querySelector(".artis_mp");
+  artisElement.innerHTML = artisData[buttonId] || "artis tidak ditemukan.";
+}
+
+var lagu = {
+  Play1: "Audio/505.m4a",
+  Play2: "Audio/ComeTogether.m4a",
+  Play3: "Audio/Sparks.m4a",
+  Play4: "Audio/Falling.m4a",
+  Play5: "Audio/Ghost.m4a",
+  Play6: "Audio/Lithium.m4a",
+  Play7: "Audio/ScottStreet.m4a",
+  Play8: "Audio/WishYouWere.m4a",
+  Play9: "Audio/Bohemian.m4a",
+};
+
+function playLagu(buttonId) {
+  var audioElement = document.getElementById("audioPlayer");
+  audioElement.src = lagu[buttonId] || "";
+  audioElement.play();
+  setInterval(updateProgressBar, 100);
+}
+
+//event
+for (let i = 1; i <= 9; i++) {
+  const playButton = document.getElementById(`Play${i}`);
+
+  playButton.addEventListener("click", function () {
+    tampilkanLirik(`Play${i}`);
+    tampilkanGambar(`Play${i}`);
+    tampilkanJudul(`Play${i}`);
+    tampilkanArtis(`Play${i}`);
+    playLagu(`Play${i}`);
+  });
+}
+
+function stopLagu() {
+  var audioElement = document.getElementById("audioPlayer");
+  audioElement.pause();
+  audioElement.currentTime = 0;
+}
+
+// Add this updated function to your existing script
+
+function toggleLagu() {
+  var audioElement = document.getElementById("audioPlayer");
+  if (audioElement.paused) {
+    audioElement.play();
+  } else {
+    audioElement.pause();
+  }
+}
+
+document.getElementById("playbut").addEventListener("click", function () {
+  toggleLagu();
+});
+
+var currentSongIndex = getRandomIndex();
+
+function getRandomIndex() {
+  return Math.floor(Math.random() * Object.keys(lagu).length);
+}
+
+function playPrevious() {
+  currentSongIndex =
+    (currentSongIndex - 1 + Object.keys(lagu).length) %
+    Object.keys(lagu).length;
+  playSelectedSong();
+}
+
+function playNext() {
+  currentSongIndex = (currentSongIndex + 1) % Object.keys(lagu).length;
+  playSelectedSong();
+}
+
+function playSelectedSong() {
+  var songKey = Object.keys(lagu)[currentSongIndex];
+  tampilkanLirik(songKey);
+  tampilkanGambar(songKey);
+  tampilkanJudul(songKey);
+  tampilkanArtis(songKey);
+  playLagu(songKey);
+}
+
+document.getElementById("prevbut").addEventListener("click", function () {
+  playPrevious();
+});
+
+document.getElementById("nextbut").addEventListener("click", function () {
+  playNext();
+});
+
+// Function to handle play/pause toggle
+function togglePlayPause(button) {
+  const allButtons = document.querySelectorAll(".lagu i.bi-play-circle-fill");
+
+  allButtons.forEach(function (otherButton) {
+    if (otherButton !== button) {
+      otherButton.classList.remove("bi-pause-circle-fill");
+      otherButton.classList.add("bi-play-circle-fill");
+      // You can add additional logic here if needed
+    }
+  });
+
+  if (button.classList.contains("bi-play-circle-fill")) {
+    button.classList.remove("bi-play-circle-fill");
+    button.classList.add("bi-pause-circle-fill");
+    // Call a function to handle play action, e.g., playLagu(button.id);
+  } else {
+    button.classList.remove("bi-pause-circle-fill");
+    button.classList.add("bi-play-circle-fill");
+    // Call a function to handle pause action, e.g., pauseLagu();
+  }
+}
+
+// Add event listeners to all play buttons in container3
+document
+  .querySelectorAll(".lagu i.bi-play-circle-fill")
+  .forEach(function (playButton) {
+    playButton.addEventListener("click", function () {
+      togglePlayPause(this);
+    });
+  });
+
+function updateProgressBar() {
+  var audioElement = document.getElementById("audioPlayer");
+  var progressBar = document.getElementById("progress");
+
+  var progress = (audioElement.currentTime / audioElement.duration) * 100;
+  progressBar.style.width = progress + "%";
+}
